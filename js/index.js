41de75bd2050055780712538c9f0dd9c6937c660
Alama1 <oldpaladin7@gmail.com>
2021-06-23 12:12:51 +0300

    Map hider added

--- js/index.js
+++ js/index.js
@@ -1,12 +1,20 @@
-// Initialize and add the map
+document.getElementById("map").style.display = "none";
 function initMap(lat, lng) {
     // The location of Uluru
     const mark = { lat: lat, lng: lng };
-    // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 8,
         center: mark,
+
     });
+    const marker = new google.maps.Marker({
+        position: mark,
+        map: map,
+    });
+}
+
+function initPoint(lat, lng) {
+    const mark = { lat: lat, lng: lng };
     const marker = new google.maps.Marker({
         position: mark,
         map: map,
